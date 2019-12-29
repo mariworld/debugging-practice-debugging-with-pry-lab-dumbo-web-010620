6ae8bbbf1337151a2bcd41dcd0b59128b5e76d53
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-29 08:26:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,7 +1,7 @@
 class GasStation
   attr_reader :brand, :unleaded_price
  
-  def initialize brand, unleaded_price
+  def initialize(brand,unleaded_price)
      @@brand = brand
      @@unleaded_price = unleaded_price
   end
